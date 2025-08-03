2a7b8c7c716eef513d6d03dbd7f8ca9b4ed42c57
Kris Kasprzak <kris.kasprzak@yahoo.com>
2025-08-03 12:51:45 -0500

    Update LTC1867.cpp

--- LTC1867.cpp
+++ LTC1867.cpp
@@ -1,158 +1,85 @@
-/*
-	The MIT License (MIT)
-
-	library writen by Kris Kasprzak
-
-	Permission is hereby granted, free of charge, to any person obtaining a copy of
-	this software and associated documentation files (the "Software"), to deal in
-	the Software without restriction, including without limitation the rights to
-	use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-	the Software, and to permit persons to whom the Software is furnished to do so,
-	subject to the following conditions:
-	The above copyright notice and this permission notice shall be included in all
-	copies or substantial portions of the Software.
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-	FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-	COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-	CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-	On a personal note, if you develop an application or product using this library and make millions of dollars, I'm happy for you!
-
-	revisions: see .h file
-*/
-
+#include <MCP3208.h>
 #include "SPI.h"
-#include "LTC1867.h"
 
-
-LTC1867::LTC1867(int CS_PIN) {
+MCP3208::MCP3208(uint8_t CS_PIN) {
 	
   cspin = CS_PIN;  
   
 }
 
-bool LTC1867::init() {
+bool MCP3208::init() {
 	
-	uint16_t test = 0;
-
-	SPI.begin();
-
+    uint16_t test = 0;
+	
 	pinMode(cspin, OUTPUT);
-
-	digitalWrite(cspin, HIGH);
-	delay(5);	
-
-	CH = CH0;	
-	UNI = UNIPOLAR;
-	SLP = AWAKE;
-	counter = 1;
-	vref = 2.5f;
 	
-	// this may not be a good test since CH0 may be grounded, but it's better than nothing...
+    	digitalWrite(cspin, HIGH);
+	
+	SPI.begin();
+	
+	// this may not be the best way but if everything is 0, maybe ADAC is not working?
 	test = analogRead(CH0);
+	test += analogRead(CH1);
+	test += analogRead(CH2);
+	test += analogRead(CH3);
+	test += analogRead(CH4);
+	test += analogRead(CH5);
+	test += analogRead(CH6);
+	test += analogRead(CH7);
 	
+	/*
+	Serial.print(test);
+	Serial.print(",");	
+	Serial.print(analogRead(CH0) * 3.3 / 4096.0, 3);
+	Serial.print(",");
+	Serial.print(analogRead(CH1) * 3.3 / 4096.0, 3);
+	Serial.print(",");
+	Serial.print(analogRead(CH2) * 3.3 / 4096.0, 3);
+	Serial.print(",");
+	Serial.print(analogRead(CH3) * 3.3 / 4096.0, 3);
+	Serial.print(",");
+	Serial.print(analogRead(CH4) * 3.3 / 4096.0, 3);
+	Serial.print(",");
+	Serial.print(analogRead(CH5) * 3.3 / 4096.0, 3);
+	Serial.print(",");
+	Serial.print(analogRead(CH6) * 3.3 / 4096.0, 3);
+	Serial.print(",");
+	Serial.print(analogRead(CH7) * 3.3 / 4096.0, 3);
+	Serial.println();
+	*/
+
 	if (test == 0){
 		return false;
-	}
-	else{
+	}	
+
+    return true;
+}
+
+
+bool MCP3208::digitalRead(uint8_t pin) {
+	if (analogRead(pin) > DIGITAL_LIMIT) {
 		return true;
 	}
-
+	return false;
 }
-
-
-
-uint16_t LTC1867::analogRead(uint8_t Channel) {
 	
-	CH = Channel;
-	buildControlByte();
-	if (counter == 1){
-		return readBits();
-	}
-	
-	else {
-		uint32_t sum = 0;
-		for (uint16_t i = 0; i < counter; i++){
-			sum = sum + (uint32_t) readBits();
-		}
-		
-		sum = sum/counter;
-		return sum;
-	}
-}
 
-
-uint16_t LTC1867::readBits() {
+uint16_t MCP3208::analogRead(uint8_t pin) {
 	
-	if (SLP == SLEEP){
-		setSleepMode(AWAKE);
-		delay(60); // data sheet says to wait 60 ms, not sure the is the correct implementation of wake
-	}
+	address = 0b01100000 | ((pin & 0b111) << 2);	
 	
-	SPI.beginTransaction(SPISettings(SPI_SPEED, MSBFIRST, SPI_MODE0));
-
-	digitalWrite(cspin, HIGH);
-	delayMicroseconds(4);
-	digitalWrite(cspin, LOW);	
-	Data[1] = SPI.transfer(CBYTE);
-	Data[0] = SPI.transfer(0x00);
-	digitalWrite(cspin, HIGH);
-	delayMicroseconds(4);
+	SPI.beginTransaction(SPISettings(SPI_SPEED, MSBFIRST, SPI_MODE0));	
+	
 	digitalWrite(cspin, LOW);
-	Data[1] = SPI.transfer(CBYTE);
-	Data[0] = SPI.transfer(0x00);
+	
+	SPI.transfer(address);
+	
+	uint8_t b1 = SPI.transfer(0x00);
+	uint8_t b2 = SPI.transfer(0x00);
+	
 	digitalWrite(cspin, HIGH);
+	
 	SPI.endTransaction();
-	return uint16_t ((Data[1] << 8) | Data[0]);
-
-}
-
-void LTC1867::buildControlByte() {
 	
-	//7			6		5		4		3			2		1		0
-	//ySD		yOS		yS1		yS0		yCOM		UNI		SLP 	not used
-	
-	// ydd is part of the ChannelData (CH)
-	// shifting everything over 1 more to start at bit 7 per the data sheet
-	CBYTE = 0;	
-	CBYTE = CH << 3;
-	CBYTE = CBYTE | (UNI << 2);
-	CBYTE = CBYTE | (SLP << 1);
-	
-}
-
-
-void LTC1867::setSleepMode(uint8_t Mode){
-	SLP = Mode;
-	if (Mode > 1){
-		SLP = 1;
-	}
-	
-}
-
-void LTC1867::setVRef(float VRef = 2.5f){
-	vref = VRef;
-	if (VRef < 0.0){
-		vref = 2.5f;
-	}
-	
-}
-		
-float LTC1867::getVolts(uint8_t Channel){
-	return analogRead(Channel) * vref / 65536.0f;
-}
-		
-
- void LTC1867::analogReadAveraging(uint16_t Counter){
-	 counter = Counter;
-	 
-	if (Counter < 1){
-		counter = 1;
-	}
-		if (Counter >1000){
-		counter = 1000;
-	}
-	
+	return (b1 << 4) | (b2 >> 4);
 }
